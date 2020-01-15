50ba8ce8c2b751067e88b2e6ee7f13acc76c9065
Ricardo Junior <ricardojunior199202@gmail.com>
2020-01-14 21:27:07 -0300

    Pagina Inicial

---
+++ src/pages/Menu/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Container } from './styles';
+import Header from '../../components/Header'
+
+export default function Menu() {
+  return (
+    <Container>
+      <Header />
+
+      <text>Bem-vindo ao serviço de listagem e filtragem de clientes e transações</text>
+
+    </Container>
+  );
+}
