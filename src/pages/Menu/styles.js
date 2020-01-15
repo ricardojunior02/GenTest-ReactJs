7c63c7bef52e1f77e786017ab462b75eaa1a653d
Ricardo Junior <ricardojunior199202@gmail.com>
2020-01-14 21:27:53 -0300

    Estilo da Pagina Inicial

---
+++ src/pages/Menu/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 100%;
+    text {
+      display: block;
+      margin-top: 360px;
+      font-size: 12px;
+      color: #333;
+      text-align: center;
+      font-size: 40px;
+    }
+`;
